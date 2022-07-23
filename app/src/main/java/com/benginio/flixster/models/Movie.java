f381c5acd4b0e3c05bbd3456fb3e155c980ef7c0
jean-baptiste benginio <jeanbaptistebenginio@gmail.com>
2022-07-23 15:27:25 -0400

    my first commit with following the videos

---
+++ app/src/main/java/com/benginio/flixster/models/Movie.java
@@ -0,0 +1,38 @@
+package com.benginio.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath=jsonObject.getString("poster_path");
+        title=jsonObject.getString("title");
+        overview=jsonObject.getString("overview");
+    }
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+            List<Movie> movie=new ArrayList<>();
+            for (int i=0; i<movieJsonArray.length(); i++){
+                movie.add(new Movie(movieJsonArray.getJSONObject(i)));
+            }
+            return movie;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w500/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
